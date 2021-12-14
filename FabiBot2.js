5265189fc119ba975bae4ba15335a4d8c02e81bc
PetusoTwo <96096173+PetusoTwo@users.noreply.github.com>
2021-12-14 15:34:34 -0500

    Update FabiBot2.js

--- FabiBot2.js
+++ FabiBot2.js
@@ -84,13 +84,13 @@ break
                 
 case 'Foto':
 conts: imagen = fs.readFileSync('./media/IMG-20211211-WA0500.jpg')
-client.sendMessage(from, image, MessageType.image)
+client.sendMessage(from, imagen, MessageType.image)
 break
 
 case 'Me lo voy a cojer': 
 conts: audio = fs.readFileSync('./media/y2meta.com - ME LA VOY A COJ3R __ Goku (320 kbps).mp3')
 client.sendMessage(from, audio, MessageType.audio,
-{quoted: sam, mimetype: 'y2meta.com - ME LA VOY A COJ3R __ Goku (320 kbps).mp3', duration: -800})
+{quoted: sam, Mimetype: 'y2meta.com - ME LA VOY A COJ3R __ Goku (320 kbps).mp3', duration: -800})
 
 }
 
